54c91b9ad1c316fd91b7108c0ab98cc088441488
a.zevajkin <mxskorpion@gmail.com>
2022-07-28 21:20:09 +0300

    task1: настроил рабочее окружение, вывел приветствие через message, подключил bootstrap

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{message}}
+  </div>
+</template>
+
+<script>
+// import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello from Vue App'
+    }
+  },
+  components: {
+    // TodoItem
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
